56f9f40fd8ef761ee754cc0406a01b33929729e0
Cong Wang <wangimagine@gmail.com>
2024-09-03 12:06:17 -0700

    Add keep-scrolling.

--- elog_crawler/app_crawl_elog.py
+++ elog_crawler/app_crawl_elog.py
@@ -37,15 +37,27 @@ def login(driver, username, password):
     submit_button.click()
 
 def scroll_to_bottom(driver):
-    SCROLL_PAUSE_TIME = 0.5
+    SCROLL_PAUSE_TIME = 1  # Increased pause time to allow content to load
     last_height = driver.execute_script("return document.body.scrollHeight")
 
     while True:
+        # Scroll down to bottom
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        # Wait to load page
         time.sleep(SCROLL_PAUSE_TIME)
+
+        # Calculate new scroll height and compare with last scroll height
         new_height = driver.execute_script("return document.body.scrollHeight")
         if new_height == last_height:
-            break
+            # If heights are the same, it's likely the end of the page
+            # Try scrolling one more time to be sure
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(SCROLL_PAUSE_TIME)
+            new_height = driver.execute_script("return document.body.scrollHeight")
+            if new_height == last_height:
+                # If still the same, we've reached the bottom
+                break
         last_height = new_height
 
 def get_element_text(parent_element, css_selector):
